92393bd206a049e0fd80ccb41173ac14993c0a27
Injun <danielmarinha@gmail.com>
2016-02-29 20:26:00 +0100

    first commit of Nyquist plotting data import OK data treatment OK TODO: plotting

--- Nyquist_get_filenames.py
+++ Nyquist_get_filenames.py
@@ -2,10 +2,13 @@ import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-get_ipython().magic('matplotlib inline')
+
 
 
 # # get temperature from filename
+
+# In[155]:
+
 # to get all filenames with extension .z in working directory
 # filenames = os.listdir()
 list_of_files = os.listdir()
@@ -16,49 +19,29 @@ for file in list_of_files:
 print(filename)
 
 
-# # import impedance files into dataframe
-file_names = []
-data_frames = {}
+# # import impedance files into dict of dataframes
+
+# In[156]:
+
+list_of_filenames = []
 column_names = ['Freq', 'Ampl', 'Bias', 'Time(Sec)', 'Real(Z)', '-Im(Z)', 'GD', 'Err', 'Range']
-for filename in os.listdir():
-    if filename.endswith('.z'):
-        temperature = str.split(filename, '.')[0]
-        file_names.append(temperature)
-        df = pd.read_csv(filename, skiprows=10, sep=',', names=column_names)
-        # df.rename(columns={0: temperature}, inplace=True)
-        data_frames[temperature] = df.abs()   # modulus of Im(Z)
+data_frames = {}
+for file in os.listdir():
+    if file.endswith('.z'):
+        filename = str.split(file, '.')[0]
+        list_of_filenames.append(filename)
+        df = pd.read_csv(file, skiprows=10, sep=',', names=column_names)
+        data_frames[filename] = df.abs()
+#         df.rename(columns={0: temperature}, inplace=True)
 
-data_frames['292'].head()
-
-
-# # Remove unused columns
-# -- there's a way to import specific columns, but can't get it to work
+# Remove unused columns
 unused_columns = ['Ampl', 'Bias', 'Time(Sec)', 'GD', 'Err', 'Range']
 for column in unused_columns:
     for df in data_frames:
         data_frames[df].drop(column, axis=1, inplace=True)
 
-data_frames['293'].head()        
 
-# # Alternative method to import csv into dataframe 
-# column_names = ['Freq', 'Ampl', 'Bias', 'Time(Sec)', 'Real(Z)', '-Im(Z)', 'GD', 'Err', 'Range']
-# frame = pd.DataFrame()
-# list_= []
-# for file in list_of_files:
-#    if file.endswith('.z'):
-#         df = pd.read_csv(file, skiprows=10, sep=',', names=column_names)
-#         list_.append(df)
-        
-#     unused_columns = ['Ampl', 'Bias', 'Time(Sec)', 'GD', 'Err', 'Range']
-#     for column in unused_columns:
-#         impedance.drop(column, axis=1, inplace=True)
-# there's a way to import specific columns, but can't get it to work
-# deleted unused colums
-# unused_columns = ['Ampl', 'Bias', 'Time(Sec)', 'GD', 'Err', 'Range']
-# for column in unused_columns:
-#     impedance.drop(column, axis=1, inplace=True)
-
-# impedance = impedance.abs()    # signal of imaginary impedance
+# In[159]:
 
 # normalize by sample dimension
 
@@ -67,122 +50,67 @@ thickness = float(input("sample thickness (cm)? "))
 geometric_factor = (np.pi*diameter**2/4)/thickness
 
 for df in data_frames:
-    data_frames[df]['Real(Z)'] /= geometric_factor
-    data_frames[df]['-Im(Z)'] /= geometric_factor
-
-data_frames['292'].head()
-
-
-# # calculate log frequency for plot labels
-
-# this will add a new column to each dataframe
-# avoids errors if measuring frequency range changes within data set
-
-for df in data_frames:
-    data_frames[df]['log_freq'] = np.log10(data_frames[df]['Freq']).round(1)
-
-data_frames['292'].head()
+    data_frames[df]['Real(Z)'] = data_frames[df]['Real(Z)'] / geometric_factor    # normalizes impedance by geometric factor
+    data_frames[df]['-Im(Z)'] = data_frames[df]['-Im(Z)'] / geometric_factor
+    data_frames[df]['log_freq'] = np.log10(data_frames[df]['Freq']).round(1)    # adds a new column to each dataframe with log(f)
+    
+# data_frames['292'].head()
 
 
-# In[215]:
+# In[160]:
 
-print(file_names)
+# Grab DataFrame rows where column has integer values
+# and creates new dataframe
 
-
-# In[225]:
-
-# TODO:
-# plot a second series in the same graph, where the points correspond to log of frequencies are close to an integer 
-# to the second decimal case
-# label those points with the integer log value
-
-# Grab DataFrame rows where column has certain values
 integers = range(-1,7,1)
 log_freq_df = {}
 
-for i in file_names:
-    for df in data_frames:
-        log_freq_df[i] = data_frames[df][data_frames[df].log_freq.isin(integers)].astype(int)
+for df in data_frames:
+    log_freq_df[df] = data_frames[df][data_frames[df].log_freq.isin(integers)]
 
 
-# In[227]:
+# In[164]:
 
-print(data_frames['292']['log_freq'])
 print(log_freq_df['292'].head())
 
 
-# In[218]:
+# # Plotting
 
-plt.scatter(data_frames['292']['Real(Z)'], data_frames['292']['-Im(Z)'])
-plt.scatter(log_freq_df['292']['Real(Z)'], log_freq_df['292']['-Im(Z)'])
+# In[172]:
+
+# to create pannel use this instead
+# fig = plt.figure()
+# for index,df in enumerate(data_frames):
+#     ax = fig.add_subplot(2,2,index+1)
 
 
-# In[204]:
-
-plt.scatter(data_frames['292']['Real(Z)'], data_frames['292']['-Im(Z)'], 
+for df in data_frames:
+    temperature = df
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(data_frames[df]['Real(Z)'], data_frames[df]['-Im(Z)'].abs(), 
             color='blue',
             alpha=0.5,
-            s=60,
-            label=temperature + ' ºC')
-plt.scatter(log_freq_df['292']['Real(Z)'], log_freq_df['292']['-Im(Z)'],
-            color="white",
-            alpha=1,
-            label='_nolegend_', 
-            s=60)
-plt.xlabel('Z\' (ohm)')
-plt.ylabel('-Z\'\' (ohm)')
-plt.title(temperature)
-plt.grid(True)
-plt.axis(xmin=0, ymin=0) 
-# plt.legend(loc=(0,0.9))
+            s=70)
+    ax.scatter(log_freq_df[df]['Real(Z)'], log_freq_df[df]['-Im(Z)'].abs(), 
+            color='white',
+            alpha=0.9,
+            s=70)
+    plt.xlabel('Z\' (ohm)')
+    plt.ylabel('-Z\'\' (ohm)')
+    plt.title(temperature)
+    plt.grid(True)
+    plt.axis(xmin=0, ymin=0) 
+    
+    # annotations
+    x_ann = log_freq_df[df]['Real(Z)'].tolist()    # gets column 'Real('Z') into a list
+    y_ann = log_freq_df[df]['-Im(Z)'].tolist()
+    label = log_freq_df[df]['log_freq'].tolist()
 
-# print log_freq as labels
-# TODO: change to annotations + arrow function
-
-x_ann = log_freq_df['292']['Real(Z)'].tolist()    # gets column 'Real('Z') into a list
-y_ann = log_freq_df['292']['-Im(Z)'].tolist()
-label = log_freq_df['292']['log_freq'].tolist()
-
-for i in range(len(x_ann)):
-    plt.text(x_ann[i], y_ann[i], label[i])
-
-
-# In[44]:
-
-fig1 = plt.figure()
-ax1 = fig1.add_subplot(111)
-ax1.plot([1, 2, 3], [1, 2, 3]);
-fig2 = plt.figure()
-ax2 = fig2.add_subplot(111)
-ax2.plot([1, 2, 3], [3, 2, 1]);
+    for i in range(len(x_ann)):
+        plt.text(x_ann[i], y_ann[i], label[i])
 plt.show()
 
-
-# In[47]:
-
-
-
-fig1 = plt.figure()
-ax1 = fig1.add_subplot(111)
-ax1.plot(impedance['Real(Z)'], impedance['-Im(Z)'].abs())
-plt.xlabel('Z\' (ohm)')
-plt.ylabel('-Z\'\' (ohm)')
-plt.title(temperature)
-plt.grid(True)
-plt.axis(xmin=0, ymin=0) 
-
-fig2 = plt.figure()
-ax2 = fig2.add_subplot(111)
-ax2.plot(impedance['Real(Z)'], impedance['-Im(Z)'].abs())
-
-plt.xlabel('Z\' (ohm)')
-plt.ylabel('-Z\'\' (ohm)')
-plt.title(temperature)
-plt.grid(True)
-plt.axis(xmin=0, ymin=0) 
-
-
-# In[ ]:
 
 
 
